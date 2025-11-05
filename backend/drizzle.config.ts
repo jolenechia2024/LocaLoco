6b49edccfa5f05408662d5a9ae3d20d852421db3
pamikalim <pamika.lim.2024@computing.smu.edu.sg>
2025-11-05 11:28:04 +0800

    drizzle

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from "drizzle-kit";
+import * as dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 export default defineConfig({
-    out: './src/drizzle',
-    schema: './src/database/schema.ts',
-    dialect: 'mysql',
-    dbCredentials: {
-        url: String(process.env.DB_URL),
-    },
-    verbose:true,
-    strict:true
+  schema: "./src/database/schema.ts",
+  out: "./drizzle",
+  dialect: "mysql",
+  dbCredentials: {
+    host: process.env.DB_HOST || "localhost",
+    port: Number(process.env.DB_PORT) || 3306,
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "wad2_project",
+  },
 });
